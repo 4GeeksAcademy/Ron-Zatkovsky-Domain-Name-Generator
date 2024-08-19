102395065d8ec60ca5146d4b9d9a48f38eb39966
RonZatkovsky <167799853+RonZatkovsky@users.noreply.github.com>
2024-08-19 18:53:17 +0000

    changed code to display the names on the website instead of the console. Also had app.js in the root directory instead of the src, now its in there.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomainName() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let item of pronoun) {
+    for (let item2 of adj) {
+      for (let item3 of noun) {
+        return item + item2 + item3 + ".com";
+      }
+    }
+  }
+}
 
 window.onload = function() {
   //write your code here
+  const query = document.getElementById("domainName");
+  query.innerHTML = generateDomainName();
   console.log("Hello Rigo from the console!");
 };
